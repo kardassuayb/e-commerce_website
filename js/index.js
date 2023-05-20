2e05c13ea38b9bc5f26c4a8ff4bbbf6c9686e21b
Suayb Kardas <kardassuayb@gmail.com>
2023-05-20 19:13:42 +0300

    slider JS completed

--- js/index.js
+++ js/index.js
@@ -21,6 +21,8 @@ document.addEventListener("click", (event)=>{
 
 //! home sidebar end
 
+//?----------------------------------------------------------------
+
 //! search modal start
 const btnOpenSearch = document.querySelector(".search-button");
 const modalSearch = document.querySelector(".modal-search");
@@ -42,4 +44,48 @@ document.addEventListener("click", (event)=>{
     }
 });
 /* click outside end */
+
 //! search modal end
+
+//?----------------------------------------------------------------
+
+//! slider start
+
+let slideIndex = 1;
+
+setInterval(() => {
+    showSlides(slideIndex += 1);
+}, 4000);
+
+function showSlides (n) {        
+    const slides = document.getElementsByClassName("slider-item");
+    const dots = document.getElementsByClassName("slider-dot");
+    
+    if ( n > slides.length ) {
+        slideIndex = 1;
+    }
+    if ( n < 1 ) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "flex";
+    dots[slideIndex -1].className+= " active";
+}
+showSlides(slideIndex);
+
+function plusSlide(n){
+    showSlides((slideIndex += n));
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+//! slider end
